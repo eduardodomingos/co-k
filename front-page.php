e6ce09945360f15b362d41a8c442ef284e45cc3c
Eduardo Domingos <epcdomingos@gmail.com>
2020-06-01 00:15:13 +0100

    Add taxonomy filters

--- front-page.php
+++ front-page.php
@@ -106,6 +106,53 @@ get_header();
 						?>
 					</div>
 					<?php
+				// Case: Works layout.
+				elseif( get_row_layout() == 'works' ):
+					$section_title = esc_html(get_sub_field('title'));
+					$sector_terms = get_terms( array(
+						'taxonomy' => 'sector',
+						'hide_empty' => false,
+					) );
+
+					$service_terms = get_terms( array(
+						'taxonomy' => 'service',
+						'hide_empty' => false,
+					) );
+
+					
+					if  ($sector_terms || $service_terms) : ?>
+						<div class="works">
+							<header class="works-header insulate--mid">
+								<div class="wrap">
+									<h2 class="screen-reader-text">Work</h2>
+									<p><?php echo $section_title; ?></p>
+									<div class="works-filters">
+										<button data-filter="*" class="active">all work</button>
+										<?php if($sector_terms): ?>
+											<div class="button-group sectors" data-filter-group="sector">
+											<span>&lt;Sectors&gt;</span>
+											<?php foreach($sector_terms as $term) : ?>
+												<button data-filter=".<?php echo str_replace(array(' ', '/','&amp;'), array('-','-','and'), strtolower($term->name)) ?>"><?php echo strtolower ($term->name); ?></button>
+											<?php endforeach;?>
+											</div>
+										<?php endif; ?>
+										<?php if($service_terms): ?>
+											<div class="button-group services" data-filter-group="service">
+												<span>services</span>
+												<?php foreach($service_terms as $term) : ?>
+													<button data-filter=".<?php echo str_replace(array(' ', '/','&amp;'), array('-','-','and'), strtolower($term->name)) ?>"><?php echo strtolower ($term->name); ?></button>
+												<?php endforeach;?>
+											</div>
+										<?php endif; ?>
+									</div>
+								</div>
+            				</header>
+						</div>
+					<?php
+					endif; ?>
+				
+					<?php
+
 				endif;
 
 			// End loop.
